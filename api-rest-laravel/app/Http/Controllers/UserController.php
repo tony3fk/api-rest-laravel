2512432f132895b46d7f3278439870c3728e60b4
tony3fk <tony3fk@yahoo.es>
2019-07-28 20:42:59 +0200

    queue antes de video 27

--- api-rest-laravel/app/Http/Controllers/UserController.php
+++ api-rest-laravel/app/Http/Controllers/UserController.php
@@ -18,25 +18,22 @@ class UserController extends Controller
         // Recoger los datos del usuario por POST
         $json = $request->input('json', null);
 
-        
-        $params_array = json_decode($json, true);//array
-        
-        $params = json_decode($json);//objeto
+        $params_array = json_decode($json, true); // array
 
-        
+        $params = json_decode($json); // objeto
 
-        if (!empty($params_array) && !empty($params)) {
-            
+        if (! empty($params_array) && ! empty($params)) {
+
             // limpiar datos
             $params_array = array_map('trim', $params_array);
-            //$params = array_map('trim', $params);
+            // $params = array_map('trim', $params);
 
             // Validar datos
 
             $validate = \Validator::make($params_array, [
                 'name' => 'required|alpha',
                 'surname' => 'required|alpha',
-                'email' => 'required|email|unique:users',   // comprobar si el usuario ya existe
+                'email' => 'required|email|unique:users', // comprobar si el usuario ya existe
                 'password' => 'required'
             ]);
 
@@ -49,13 +46,14 @@ class UserController extends Controller
                     'errors' => $validate->errors()
                 );
             } else {
-                
-                //validación pasada correctamente
+
+                // validación pasada correctamente
                 // cifrar la contraseña
-                
-                $pwd = password_hash($params->password, PASSWORD_BCRYPT, ['cost' => 4]);
-                
-                
+
+                $pwd = password_hash($params->password, PASSWORD_BCRYPT, [
+                    'cost' => 4
+                ]);
+
                 // crear el usuario
                 $user = new User();
                 $user->name = $params_array['name'];
@@ -63,37 +61,196 @@ class UserController extends Controller
                 $user->email = $params_array['email'];
                 $user->password = $pwd;
                 $user->role = 'ROLE_USER';
-                
-               
-                
-              //guardar el usuario
-              $user->save();
+
+                // guardar el usuario
+                $user->save();
                 $data = array(
                     'status' => 'success',
                     'code' => 200,
                     'message' => 'El usuario se ha creado correctamente',
                     'user' => $user
-                    
                 );
             }
-        }else{
+        } else {
             $data = array(
                 'status' => 'error',
                 'code' => 404,
                 'message' => 'Los datos enviados no son correctos'
-                
             );
         }
-
-        
 
         return response()->json($data, $data['code']);
     }
 
     public function login(Request $request)
     {
-        $JwtAuth=new \JwtAuth();
-        echo $JwtAuth->signup();
-        return "Acción de login de usuarios";
+        $JwtAuth = new \JwtAuth();
+
+        // recibir datos por POST
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+
+        // Validar esos datos
+        $validate = \Validator::make($params_array, [
+            'email' => 'required|email', // comprobar si el usuario ya existe
+            'password' => 'required'
+        ]);
+
+        if ($validate->fails()) {
+        //si la validacion falla
+            $signup = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'El usuario no se ha podido loguear',
+                'errors' => $validate->errors()
+            );
+        } else {
+
+            // validación pasada correctamente
+            // cifrar la contraseña
+
+            $pwd = hash('sha256', $params->password);
+
+            // Devolver token o datos
+
+            $signup = $JwtAuth->signup($params->email, $pwd);
+
+            if (!empty($params->gettoken)) {
+                $signup = $JwtAuth->signup($params->email, $pwd, true);
+            }
+        }
+        return response()->json($signup, 200);
+        //var_dump($params_array);
+        
+        //die();
     }
 }
+
+
+/*namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function pruebas(Request $request){
+        return "Accion de pruebas de User - Controller";
+    }
+    
+    public function register(Request $request)
+    {
+        //Recoger los datos del usuario por post********************************
+        $json = $request->input('json', null);
+        $params = json_decode($json); //Objeto
+        //var_dump($params->name); die(); este es el objeto especifico que se obtiene
+        $params_array = json_decode($json, true); //Array
+        //var_dump($params_array); die(); esta es la forma de obtenerlo en un array
+        
+        //Validar datos vacios**************************************************
+        if(!empty($params) && !empty($params_array))
+        {
+            //Limpiar datos*****************************************************
+            $params_array = array_map('trim', $params_array);
+            
+            //Validar datos ****************************************************
+            $validate = \Validator::make($params_array, [
+                'name'      =>  'required|alpha',
+                'surname'   =>  'required|alpha',
+                'email'     =>  'required|email|unique:users',
+                'password'  =>  'required'
+            ]);
+            
+            if($validate->fails())
+            {
+                //La validacion ha fallado
+                $data = Array(
+                    'status'    =>  'error',
+                    'code'      =>  404,
+                    'message'   =>  'El usuario no se ha creado',
+                    'errors'    =>  $validate->errors()
+                    );
+            }
+            else
+            {
+                //Validacion pasada correctamente
+                //Cifrar la contraseña******************************************
+                // $pwd = password_hash($params->password, PASSWORD_BCRYPT, ['cost' => 4]);  //Este es un sistema de cifrado que varia la password
+                $pwd = hash('sha256', $params->password);
+                
+                //Crear al usuario**********************************************
+                $user = new User();
+                $user->name     = $params_array['name'];
+                $user->surname  = $params_array['surname'];
+                $user->email    = $params_array['email'];
+                $user->password = $pwd;
+                $user->role     = 'ROLE_USER';
+                
+                //Guardar el usuario********************************************
+                $user->save();
+                
+                $data = Array(
+                    'status'    =>  'success',
+                    'code'      =>  200,
+                    'message'   =>  'El usuario se ha creado correctamente',
+                    'user'      =>  $user
+                    );
+            }
+        }
+        else
+        {
+            $data = Array(
+                'status'    =>  'error',
+                'code'      =>  404,
+                'message'   =>  'Los datos enviados no son correctos'
+                );
+        }
+        
+        return response()->json($data, $data['code']);
+    }
+    
+    
+    public function login(Request $request)
+    {
+        $jwtAuth = new \JwtAuth();
+        
+        // Recibir datps por post
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+        // var_dump($params_array);
+        // die();
+        // Validar esos datos
+        $validate = \Validator::make($params_array, [
+            'email'     =>  'required|email',
+            'password'  =>  'required'
+        ]);
+        
+        if ($validate->fails())
+        {
+            //La validacion ha fallado
+            $signup = array(
+                'status'    =>  'error',
+                'code'      =>  404,
+                'message'   =>  'El usuario no se ha podido identificar',
+                'errors'    =>  $validate->errors()
+            );
+        }else
+        {
+            // Cifrar la password
+            $pwd = hash('sha256', $params->password);
+            // Devolver token o datos
+            $signup = $jwtAuth->signup($params->email, $pwd);
+            
+            if(!empty($params->gettoken))
+            {
+                $signup = $jwtAuth->signup($params->email, $pwd, true);
+            }
+        }
+        
+        return response()->json($signup, 200);
+    }
+    
+}/*
+?>
--- api-rest-laravel/app/helpers/JwtAuth.php
+++ api-rest-laravel/app/helpers/JwtAuth.php
@@ -1,26 +1,64 @@
 <?php
 namespace App\Helpers;
+
 use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\DB;
 use App\User;
 
-class JwtAuth{
-    
-    
-    public function signup(){
-        //buscar si existe el usuario con sus credenciales
-        
-        //comprobar si son correctas (objeto)
-        
-        
-        //generar el token con los datos del usuario identificado
-        
-        
-        //devolver los datos decodificados o el token, en funcion de un parametro
-        
-        return 'Método de la case JwtAuth';
+class JwtAuth
+{
+
+    public function signup($email, $password, $gettoken = null)
+    {
+        // buscar si existe el usuario con sus credenciales
+        $user = User::where([
+            'email' => $email,
+            'password' => $password
+        ])->first();
+
+        // comprobar si son correctas (objeto)
+        $signup = false;
+        if (is_object($user)) {
+            $signup = true;
+        }
+
+        // generar el token con los datos del usuario identificado
+        if ($signup) {
+            $token = array(
+                'sub' => $user->id,
+                'email' => $user->email,
+                'name' => $user->name,
+                'surname' => $user->surname,
+                'iat' => time(),
+                'exp' => time() + (7 * 24 * 60 * 60)
+            );
+            
+
+            $jwt = JWT::encode($token, $this->key, 'HS256');
+            $decoded = JWT::decode($jwt, $this->key, [
+                'HS256'
+            ]);
+            
+            // devolver los datos decodificados o el token, en funcion de un parametro
+            if (is_null($gettoken)) {
+                $data = $jwt;
+            } else {
+                $data = $decoded;
+            }
+        }else{
+            $data=array(
+                'status'=>'error',
+                'message'=>'login incorrecto'
+            );
+            
+        }
+
+        return $data;
+        //var_dump($data);
+        //die();
+
+       
     }
-    
 }
 
 ?>
