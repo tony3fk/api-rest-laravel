9f09f600a85e0e8f3e5e950383f2242d9045e759
tony3fk <tony3fk@yahoo.es>
2019-08-15 20:55:54 +0200

    Actualizacion de categorias clase 38 ok

--- api-rest-laravel/app/Http/Controllers/CategoryController.php
+++ api-rest-laravel/app/Http/Controllers/CategoryController.php
@@ -92,4 +92,41 @@ class CategoryController extends Controller
         // devolver el resultado
         return response()->json($data, $data['code']);
     }
+    
+    public function update($id, Request $request){
+        
+        //recoger datos por post
+        $json=$request->input('json', null);
+        $params_array=json_decode($json, true);
+        
+        if(!empty($params_array)){
+            //validar datos
+            $validate=\Validator::make($params_array, [
+                'name'=>'required'
+            ]);
+            //quitar lo que no quiero actualizar
+            unset($params_array['id']);
+            unset($params_array['created_at']);
+            //actualizar el registro categoria
+            
+            $category=Category::where('id', $id)->update($params_array);
+            $data = array(
+                'code' => 200,
+                'status' => 'success',
+                'category' => $params_array
+            );
+            
+            
+            
+        } else {
+            $data = array(
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No has enviado ninguna categoria'
+            );
+        }
+        
+        //devolver respuesta
+        return response()->json($data, $data['code']);
+    }
 }
