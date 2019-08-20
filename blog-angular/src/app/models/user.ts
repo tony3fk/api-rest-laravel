c6f48126ecb240744f9a0c3d5940c2b6b01b362c
tony3fk <tony3fk@yahoo.es>
2019-08-20 11:26:03 +0200

    crear modelos ok, clase 53 ok

---
+++ blog-angular/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class user{
+	constructor(
+		public id: number,
+		public name: string,
+		public surname: string,
+		public role: string, 
+		public email: string,
+		public password: string,
+		public description: string,
+		public image: string
+
+	){}
+}
