9f600bc2e46fc7c3a906705e37a14ad4a2e5da06
tony3fk <tony3fk@yahoo.es>
2019-08-21 10:44:29 +0200

    componente de registro, clase 56 ok

--- blog-angular/src/app/app.module.ts
+++ blog-angular/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
