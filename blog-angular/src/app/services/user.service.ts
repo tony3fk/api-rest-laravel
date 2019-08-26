62ca3d181dd81f5f87260df3b1d64b2eeaac441e
tony3fk <tony3fk@yahoo.es>
2019-08-26 21:27:32 +0200

    user.service.ts fixed User class

--- blog-angular/src/app/services/user.service.ts
+++ blog-angular/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { user } from '../models/user';
+import { User } from '../models/user';
 //import { TestBed } from '@angular/core/testing';
 import { global } from './global';
 
