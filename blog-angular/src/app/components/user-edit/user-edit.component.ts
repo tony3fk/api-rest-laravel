cafb7c170018bdf1365c28cf11eeb0822f42cd18
tony3fk <tony3fk@yahoo.es>
2019-08-28 21:06:26 +0200

    clase 73 NOK

--- blog-angular/src/app/components/user-edit/user-edit.component.ts
+++ blog-angular/src/app/components/user-edit/user-edit.component.ts
@@ -14,6 +14,9 @@ export class UserEditComponent implements OnInit {
   public user: User;
   public identity;
   public token;
+  public status;
+
+
 
   constructor(
     private _userService: UserService
@@ -23,10 +26,58 @@ export class UserEditComponent implements OnInit {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     //rellenar objeto usuario
-    this.user = this.identity;
+    this.user = new User(
+      this.identity.sub,
+      this.identity.name,
+      this.identity.surname,
+      this.identity.role,
+      this.identity.email,
+      '',
+      this.identity.description,
+      this.identity.image
+    );
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form) {
+    this._userService.update(this.token, this.user).subscribe(
+      response => {
+        if (response && response.status) {
+          console.log(response);
+          this.status = 'success';
+
+          //actualizar usuario en sesion
+          if (response.changes.name) {
+            this.user.name = response.changes.name;
+          }
+          if (response.changes.surname) {
+            this.user.surname = response.changes.surname;
+          }
+          if (response.changes.email) {
+            this.user.email = response.changes.email;
+          }
+          if (response.changes.description) {
+            this.user.description = response.changes.description;
+          }
+          if (response.changes.image) {
+            this.user.image = response.changes.image;
+          }
+
+
+          this.identity = this.user;
+          localStorage.setItem('identity', JSON.stringify(this.identity));
+
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    );
   }
 
 }
--- blog-angular/src/app/services/user.service.ts
+++ blog-angular/src/app/services/user.service.ts
@@ -45,6 +45,16 @@ export class UserService {
         return this._http.post(this.url + 'login', params, { headers: headers });
     }
 
+    update(token, user): Observable<any> {
+
+        let json = JSON.stringify(user);
+        let params = "json=" + json;
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded').set('Authorization', token);
+
+        return this._http.put(this.url + 'user/update', params, { headers: headers });
+
+    }
+
     getIdentity() {
         let identity = JSON.parse(localStorage.getItem('identity'));
         if (identity && identity != 'undefined') {
